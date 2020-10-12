0af5f40e6912fe6be48aaedae74e1e198a9c1542
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-12 17:07:00 +0530

    [Prerna]ADDED: Test case to check size when larger tree created

--- src/test/java/com/bridgelabz/DataStructure/MyBinaryTreeTest.java
+++ src/test/java/com/bridgelabz/DataStructure/MyBinaryTreeTest.java
@@ -15,5 +15,25 @@ public class MyBinaryTreeTest {
 		int size = myBinaryTree.getSize();
 		assertEquals(3, size);
 	}
+	
+	@Test
+	public void given13Numbers_whenAddedToBinarySearchTree_shouldReturnSizeThirteen() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		assertEquals(13, size);
+	}
 
 }
