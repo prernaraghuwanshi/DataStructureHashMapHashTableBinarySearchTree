219af843dfa18449cd2778d7352bcc626c23e701
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-13 09:36:48 +0530

    [Prerna]REFACTORED: LinkedHashMap Test case generalized with @Before

--- src/test/java/com/bridgelabz/DataStructure/LinkedHashMapTest.java
+++ src/test/java/com/bridgelabz/DataStructure/LinkedHashMapTest.java
@@ -2,15 +2,17 @@ package com.bridgelabz.DataStructure;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LinkedHashMapTest {
+	LinkedHashMap<String, Integer> myLinkedHashMap;
 
-	@Test
-	public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidFrequency() {
+	@Before
+	public void initialize() {
 		String sentence = "Paranoids are not paranoid because they are paranoid but because "
 				+ "they keep putting themselves deliberately into paranoid avoidable situations";
-		LinkedHashMap<String, Integer> myLinkedHashMap = new LinkedHashMap<>();
+		myLinkedHashMap = new LinkedHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
 			Integer value = myLinkedHashMap.get(word);
@@ -20,24 +22,16 @@ public class LinkedHashMapTest {
 				value += 1;
 			myLinkedHashMap.add(word, value);
 		}
+	}
+
+	@Test
+	public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidFrequency() {
 		int frequency = myLinkedHashMap.get("paranoid");
 		assertEquals(3, frequency);
 	}
 
 	@Test
 	public void givenASentence_whenWordIsRemoved_shouldReturnDeletedWordFrequency() {
-		String sentence = "Paranoids are not paranoid because they are paranoid but because "
-				+ "they keep putting themselves deliberately into paranoid avoidable situations";
-		LinkedHashMap<String, Integer> myLinkedHashMap = new LinkedHashMap<>();
-		String[] words = sentence.toLowerCase().split(" ");
-		for (String word : words) {
-			Integer value = myLinkedHashMap.get(word);
-			if (value == null)
-				value = 1;
-			else
-				value += 1;
-			myLinkedHashMap.add(word, value);
-		}
 		int frequency = myLinkedHashMap.remove("avoidable");
 		assertEquals(1, frequency);
 	}
